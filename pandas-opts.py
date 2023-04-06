8fc38a007c7f3c1a17f89feb2ae7c3d906026e5a
Vijay Lulla <vijay_lulla@sweetwater.com>
2023-04-06 14:35:13 -0400

    Pandas options to help interactive use

--- pandas-opts.py
+++ pandas-opts.py
@@ -1,11 +1,17 @@
 ## set in ~/.ipython/profile_default/startup/01-pandas-options.py
 ## See ipython --help-all to read about profiles.
+## Also try `pd.describe_option()` in your repl
 
 import pandas as pd
 
-pd.set_option("display.width",120)
+## pd.set_option("display.width",120) ## set this from what is reported by `tput cols`
 pd.set_option("display.max_rows",6)
 pd.set_option("display.max_colwidth",50)
 pd.set_option("display.max_seq_items",20)
 pd.set_option("display.html.table_schema",True)
+pd.set_option("display.date_yearfirst",True)
+pd.set_option("display.memory_usage","deep")
+pd.set_option("display.precision",3)
+pd.set_option("display.float_format",lambda x: f"{x:_}") ## aids me with larger numbers
+pd.set_option("display.show_dimensions",True)
 pd.set_option("mode.copy_on_write",True) ## introduced in pandas 2.0.0
