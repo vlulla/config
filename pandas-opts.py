3ae3324de781975ab2fe5f5b5e5fa25f4e638d05
Vijay Lulla <vijay_lulla@sweetwater.com>
2024-10-10 10:42:59 -0400

    Use full width of terminal to show variable content

--- pandas-opts.py
+++ pandas-opts.py
@@ -2,11 +2,12 @@
 ## See ipython --help-all to read about profiles.
 ## Also try `pd.describe_option()` in your repl
 
-import pandas as pd
+import pandas as pd, numpy as np, subprocess
+twidth = int(subprocess.check_output("tput cols", shell=TRUE))-20
 
 ## pd.set_option("display.width",120) ## set this from what is reported by `tput cols`
 pd.set_option("display.max_rows",6)
-pd.set_option("display.max_colwidth",50)
+pd.set_option("display.max_colwidth", twidth)
 pd.set_option("display.max_seq_items",20)
 pd.set_option("display.html.table_schema",True)
 pd.set_option("display.date_yearfirst",True)
@@ -15,3 +16,4 @@ pd.set_option("display.precision",3)
 pd.set_option("display.float_format",lambda x: f"{x:_}") ## aids me with larger numbers
 pd.set_option("display.show_dimensions",True)
 pd.set_option("mode.copy_on_write",True) ## introduced in pandas 2.0.0
+np.set_printoptions(linewidth=twidth)
