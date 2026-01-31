e73a95fa207f1266b921ee09f2c39213a70e9c30
Vijay Lulla <vijaylulla@gmail.com>
2026-01-31 14:06:22 -0500

    Do not raise exceptions in ipython in environments which do not have polars installed!

--- polars-opts.py
+++ polars-opts.py
@@ -1,6 +1,10 @@
-import polars as pl
+import sys
+try:
+  import polars as pl
 
-pl.Config.set_tbl_column_data_type_inline(True)
-pl.Config.set_tbl_dataframe_shape_below(True)
-pl.Config.set_tbl_formatting(format="NOTHING")
-pl.Config.set_tbl_hide_column_data_types(True)
+  pl.Config.set_tbl_column_data_type_inline(True)
+  pl.Config.set_tbl_dataframe_shape_below(True)
+  pl.Config.set_tbl_formatting(format="NOTHING")
+  pl.Config.set_tbl_hide_column_data_types(True)
+except ModuleNotFoundError as e:
+  print(f"{e=}", file=sys.stderr)
