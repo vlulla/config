831b97ebfa9483b71c5f4dadb8745f566febcdce
Vijay Lulla <vijay_lulla@sweetwater.com>
2026-02-02 10:47:47 -0500

    Enable polars df formatting.
    Since I use polars, duckdb, and pandas interchangeably in an interactive
    Python session the formatting helps me distinguish between these
    different formats.  I find this invaluable in trying to determine what
    idioms i have to use for a particular dataframe object!

--- polars-opts.py
+++ polars-opts.py
@@ -4,7 +4,7 @@ try:
 
   pl.Config.set_tbl_column_data_type_inline(True)
   pl.Config.set_tbl_dataframe_shape_below(True)
-  pl.Config.set_tbl_formatting(format="NOTHING")
-  pl.Config.set_tbl_hide_column_data_types(True)
+  ## pl.Config.set_tbl_formatting(format="NOTHING")
+  ## pl.Config.set_tbl_hide_column_data_types(True)
 except ModuleNotFoundError as e:
   print(f"{e=}", file=sys.stderr)
